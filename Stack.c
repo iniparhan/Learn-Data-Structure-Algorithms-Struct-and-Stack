65bb5a294563d3e803e976e4db76568f50bd25cb
Ahmad Farhan <105894704+iniparhan@users.noreply.github.com>
2024-04-02 21:11:20 +0700

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -1,36 +1,61 @@
 #include <stdio.h>
 
+// Fungsi twoStacks untuk menghitung jumlah elemen maksimum yang bisa diambil
 int twoStacks(int maxSum, int array_1[], int banyak_array_1, int array_2[], int banyak_array_2) {
-    int sum=0;  // Variabel Total angka
-    int count=0;    // Variabel banyaknya angka dengan total sama dengan maxSum
-    int i=0;  
-    int j=0;   
+    int sum = 0;
+    int count = 0;
+    int i = 0;
+    int j = 0;
 
     // Loop untuk menjumlah angka di dalam array pertama, dan akan trus berjalan jika kurang dari banyak_array_1 dan blum lebih dari maxSum
-    while(i<banyak_array_1 && sum+array_1[i]<=maxSum) {   
-        sum += array_1[i];    // Menjumlahkan sum dengan array pertama
+    printf("Langkah 1: mengambil elemen dari tumpukan A hingga melebihi max jumlah atau mencapai akhir:\n");
+    while(i < banyak_array_1 && sum + array_1[i] <= maxSum) {
+        sum += array_1[i];  // Menjumlahkan sum dengan array pertama
+        printf("%d ", array_1[i]); 
         i++;
     }
+    count = i; // Jumlah elemen yang diambil dari tumpukan A
 
-    count=i; // Menginisialisasikan Variabel baru untuk menghitung banyaknya angka yang ketika di jumlah hasilnya tidak melebihi maxSum
+    printf("\nTumpukan A: ");
+    for(int a = 0; a < i; a++){
+        printf("%d ", array_1[a]);
+    }
+    printf("\nTumpukan B: ");
+    for(int b = 0; b < j; b++){
+        printf("%d ", array_2[b]);
+    }
+    printf("\n\n");
 
     // Loop untuk Menjumlahkan sum dengan array kedua, dan akan trus berjalan jika j kurang dari banyak_array_2 dan i lebih dari sama dengan nol
-    while(j<banyak_array_2 && i>=0) {   
-        sum += array_2[j];    // Menjumlahkan sum dengan array kedua
+    while(j < banyak_array_2 && i >= 0) {
+        sum += array_2[j];  // Menjumlahkan sum dengan array kedua
         j++;
 
         // Loop untuk mengurangkan sum dengan array sesuai dengan i supaya tidak lebih dari maxSum
-        while(sum>maxSum && i>0) {
+        while(sum > maxSum && i > 0) {
             i--;
             sum -= array_1[i];
         }
-
-        // Membandingkan jika sum jika kurang sama dengan dari maxSum dan i + j lebih dari count, maka akan menjumlah i dan j dalam variabel count
-        if(sum<=maxSum && i+j>count)
+        
+         // Membandingkan jika sum jika kurang sama dengan dari maxSum dan i + j lebih dari count, maka akan menjumlah i dan j dalam variabel count
+        if(sum <= maxSum && i + j > count) {
             count = i + j;
+        }
+
+        // mengambil elemen dari tumpukan B
+        printf("Langkah %d: mengambil elemen dari tumpukan B hingga melebihi max jumlah atau mencapai akhir\n", j + 1);
+        printf("Tumpukan A: ");
+        for(int a = 0; a < i; a++){
+            printf("%d ", array_1[a]);
+        }
+        printf("\nTumpukan B: ");
+        for(int b = 0; b < j; b++){
+            printf("%d ", array_2[b]);
+        }
+        printf("\n\n");
     }
 
-    return count;   // Mengembalikan Nilai Count
+    return count;
 }
 
 int main() {
